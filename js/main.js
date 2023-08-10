65b7d76cf40c93892216ef129b9c0f0c47f5320b
Yourfirstname Yourlastname <your-email@example.com>
2023-08-10 18:31:05 +1000

    Added html & css

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log('hanna raini', axios);
+const apiKey= "AIzaSyC3T-tPbe_9EhXXwJ1BzCxiY5LRSfjySYw";
+
+axios.get(`https://www.googleapis.com/youtube/v3/search?q=music&key=${apiKey}`).then(function(res){
+  console.log(res.data);
+}).catch(err=> {
+  console.log('Error', err);
+});
