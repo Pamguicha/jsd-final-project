1f5594c51406486c78961c75286e89a7fd7e6b27
Yourfirstname Yourlastname <your-email@example.com>
2023-08-13 11:06:07 +1000

    Added changes values to string, changed html.

--- js/main.js
+++ js/main.js
@@ -2,26 +2,83 @@
 console.log('hello dogs', axios);
 
 const apiKey = 'J0GvRGzkHEE/3HmmtvTKJA==pXp4BWMraSlgHrMp';
+const formSearch = document.querySelector("#searchForm");
 const resultsParent = document.querySelector('#resultsPage');
 
 
+const searchDog = (event) => {
+  event.preventDefault();
+
+const dogName = document.querySelector("#dogQuery").value;
 
 axios.get('https://api.api-ninjas.com/v1/dogs', {
   params: {
-    name: 'beagle'
-  },
+    name: `${dogName}`
+    },
   headers: {
     'X-Api-Key': `${apiKey}`
-  }
-})
-.then(function(res) {
+    }
+  })
+.then(res => {
+  resultsParent.innerHTML = '';
+  renderSearchResult(res.data[0]);
   console.log(res.data[0]);
-})
-.catch(function(err) {
-  console.log('Error:', err);
-});
+  })
+.catch(err => {
+  console.log('Error loading search results', err);
+  });
+
+};
+
+const valueLevelToString = (level) => {
+  switch (level) {
+    case 1:
+     return "Low level";
+     break;
+    case 2:
+     return "Low-medium level";
+     break;
+    case 3:
+     return "Medium level";
+     break;
+    case 4:
+     return "Medium-high";
+     break;
+    case 5:
+    return "Excellent";
+    default: 
+    return "Unknown level";
+  
+  }
+
+};
 
 
+const renderSearchResult = (breed) => {
+
+  const divTag = document.createElement('div');
+  divTag.innerHTML = `
+  <h1 class="dog"> ${breed.name} </h1>
+  <img class="mainDog" src= "${breed.image_link}" />
+  <ol> 
+  <li> <strong>Energy Level:</strong> ${valueLevelToString(breed.energy)}. </li>
+  <li> <strong>Good with kids:</strong> ${valueLevelToString(breed.good_with_children)}. </li>
+  <li> <strong>Sociable with dogs:</strong> ${valueLevelToString(breed.good_with_other_dogs)} </li>
+  <li> <strong>Playfulness Level:</strong> ${valueLevelToString(breed.playfulness)}. </li>
+  <li> <strong>Protectiveness:</strong> ${valueLevelToString(breed.protectiveness)}. </li>
+  <li> <strong>Grooming Level:</strong> ${valueLevelToString(breed.grooming)}. </li>
+  <li> <strong>Maximum Life Expectancy:</strong> ${breed.max_life_expectancy} years old. </li>
+  <li> <strong>Minimum Life Expectancy:</strong> ${breed.min_life_expectancy} years old. </li>
+  <li> <strong>Barking Level:</strong> ${valueLevelToString(breed.barking)}. </li>
+  <li> <strong>Trainability Level:</strong> ${valueLevelToString(breed.trainability)}. </li>
+   
+  </ol>
+  `
+  resultsParent.appendChild(divTag);
+};
+
+
+formSearch.addEventListener("submit", searchDog);
 
 //Name dog: res.data[0].name
 //life expectansi: res.data[0].max_life_expectancy
